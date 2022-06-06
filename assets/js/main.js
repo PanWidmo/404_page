173b72f8496b698f4a9a7d582f0d99d5531d7a47
PanWidmo <tomasz.baranww@gmail.com>
2022-06-06 13:08:37 +0200

    Hide menu when clicking on link

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,14 @@ if (navClose) {
     navMenu.classList.remove("show-menu");
   });
 }
+
+/* HIDE MENU MOBILE */
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.querySelector("#nav-menu");
+  //When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((x) => x.addEventListener("click", linkAction));
